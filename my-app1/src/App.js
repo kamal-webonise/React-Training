7b719ac836c6c21de38c62ff951cc6100e136eb2
kamal-webonise <kamal.singh@weboniselab.com>
2018-04-11 14:35:40 +0530

    React SetUp

---
+++ my-app1/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <span>Name </span>
+        <span>Kamal Singh</span> 
+      </div>  
+    );
+  }
+}
+
+export default App;
